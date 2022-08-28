47fcb5232005df5991fec768de7fe2787998c3c7
Krishna Gole <45708333+KrishnaGole@users.noreply.github.com>
2022-08-28 21:56:25 +0530

    Update script.js

--- javascript/04/fizzbuzz/script.js
+++ javascript/04/fizzbuzz/script.js
@@ -13,5 +13,30 @@ const fizzbuzz = document.getElementById('fizzbuzz')
  */
 
 print.onclick = function () {
-
+  for(int i = 1; i <= count; i++){
+      if(i%15 == 0){
+          var ul = document.getElementById("fizzbuzz");
+          var li = document.createElement("li");
+          li.appendChild(document.createTextNode("fizzbuzz"));
+          ul.appendChild(li);
+      }
+      else if(i%5 == 0){
+          var ul = document.getElementById("fizzbuzz");
+          var li = document.createElement("li");
+          li.appendChild(document.createTextNode("fizz"));
+          ul.appendChild(li);
+      }
+      else if(i%3 == 0){
+          var ul = document.getElementById("fizzbuzz");
+          var li = document.createElement("li");
+          li.appendChild(document.createTextNode("buzz"));
+          ul.appendChild(li);
+      }
+      else{
+          var ul = document.getElementById("fizzbuzz");
+          var li = document.createElement("li");
+          li.appendChild(document.createTextNode(i));
+          ul.appendChild(li);
+      }
+  }
 }
